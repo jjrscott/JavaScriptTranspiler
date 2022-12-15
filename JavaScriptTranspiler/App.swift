b0fddec41f69e5727fc39aef21af8546bd45840c
John Scott <john@jjrscott.com>
2022-12-15 17:30:09 +0000

    Remove AppKit code

---
+++ JavaScriptTranspiler/App.swift
@@ -0,0 +1,15 @@
+//
+//  App.swift
+//  JavaScriptTranspiler
+//
+//  Created by John Scott on 15/12/2022.
+//
+
+import Cocoa
+
+@main
+class MyApp {
+    static func main() {
+        print("Hello")
+    }
+}
